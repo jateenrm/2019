1740ce93969691fbc915577573698f9bd89ea9e7
Jateen <jateenrm@yahoo.com>
2019-06-17 19:01:39 +0000

    ok

--- 16.go
+++ 16.go
@@ -11,13 +11,13 @@ const (
 )
 
 func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float 64  {
+func needFloat(x float64) float64  {
         return x * 0.1
     
 }
 
 func main() {
-        fmt.Println(needInt(small))
-        fmt.Println(needFloat(small))
+        fmt.Println(needInt(Small))
+        fmt.Println(needFloat(Small))
         fmt.Println(needFloat(Big))
 }
